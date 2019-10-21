6535f3dd2ed3a77378e41b762f32fa2f686909d9
nicolevegai <mvega.ieu2017@student.ie.edu>
2019-10-21 11:33:41 +0200

    first Commit

---
+++ src/LinkDictionary.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.*;
+import java.io.*;
+
+public class LinkDictionary {
+
+        public static void main(String args[])throws IOException{
+            String content = new String();
+            int count=1;
+            File file = new File("unsorteddict.txt");
+            LinkedList<String> list = new LinkedList<String>();
+
+            try {
+                Scanner sc = new Scanner(new FileInputStream(file));
+                while (sc.hasNextLine()){
+                    content = sc.nextLine();
+                    list.add(content);
+                }
+                sc.close();
+            }catch(FileNotFoundException fnf){
+                fnf.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("\nProgram terminated Safely...");
+            }
+
+            Collections.reverse(list);
+            Iterator i = list.iterator();
+            while (i.hasNext()) {
+                System.out.print("Node " + (count++) + " : ");
+                System.out.println(i.next());
+            }
+        }
+    }
+
