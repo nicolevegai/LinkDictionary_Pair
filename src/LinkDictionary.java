034273dc90d7474c10786bcf5e45371140a9e722
nicolevegai <mvega.ieu2017@student.ie.edu>
2019-10-25 12:54:23 +0200

    Creation of new file

--- src/LinkDictionary.java
+++ src/LinkDictionary.java
@@ -13,55 +13,7 @@ public class LinkDictionary {
 
     public static void main(String args[]) {
 
-        BufferedReader reader;
-        LinkedList<String> dict = new LinkedList<String>();
-        try {
-            reader = new BufferedReader(new FileReader("/Users/nicolevega/Desktop/prueba.txt"));//
-            String line = reader.readLine();
-            while (line != null) {
-                if (dict.size() == 0) {
-                    dict.add(line);
-                } else {
-                    if (!sortList(line, dict)) {
-                        dict.add(line);
-                    }
-
-                }
-                line = reader.readLine();
-            }
-            reader.close();
-            BufferedWriter writer = new BufferedWriter(new FileWriter("sorteddict.txt"));
-            for (int i = 0; i < dict.size(); i++) {
-                out.println(dict.get(i));
-                writer.write(dict.get(i).toString());
-                writer.newLine();
-            }
-
-            writer.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        }
     }
-    public static boolean sortList(String inputvalue, List list){
-        boolean retorno = false;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).toString().toLowerCase().compareTo(inputvalue.toLowerCase()) > 0) {
-                if(i == 0){
-                    list.add(0, inputvalue);
-                }else{
-                    list.add(i, inputvalue);
-                }
-                retorno = true;
-                break;
-            }
-        }
-        return retorno;
     }
-
-}
-
-
 
 
